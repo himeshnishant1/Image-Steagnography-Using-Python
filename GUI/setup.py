3e4fd272b7b71b642fa191ef708b8b7de9c8814b
himeshnishant1 <himeshnishant1@gmail.com>
2020-04-24 11:54:49 +0530

    updated your mistake

--- GUI/setup.py
+++ GUI/setup.py
@@ -1,18 +1,17 @@
-import cx_Freeze
 import sys
-import matplotlib
+from cx_Freeze import setup, Executable
 
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
 base = None
-
-if sys.platform == 'win32':
+if sys.platform == "win32":
     base = "Win32GUI"
 
-executables = [cx_Freeze.Executable("login_text.py", base=base, icon="clienticon.ico")]
-
-cx_Freeze.setup(
-    name = "SeaofBTC-Client",
-    options = {"build_exe": {"packages":["tkinter","matplotlib"], "include_files":["clienticon.ico"]}},
-    version = "0.01",
-    description = "Sea of BTC trading application",
-    executables = executables
-    )
+setup(  name = "guifoo",
+        version = "0.1",
+        description = "My GUI application!",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("login_text.py", base=base)])
