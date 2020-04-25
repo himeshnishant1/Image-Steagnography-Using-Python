afd834842e19a36720cc54d74d1e1d5c40c7e7a5
himeshnishant1 <himeshnishant1@gmail.com>
2020-04-25 12:55:57 +0530

    doc added of 90mb zip file

--- GUI/setup.py
+++ GUI/setup.py
@@ -1,17 +1,17 @@
-import sys
-from cx_Freeze import setup, Executable
+from cx_Freeze import setup,Executable
 
-# Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+includefiles = ['encoder_bcps.py', 'admin_login.py', 'Admin_pannel.py', 'data_insert.py', 'data_VIEW.py', 'database.py', 'decoder_BCPS.py',
+                'decoder_LSB.py', 'decoder.py', 'encoder_LSB.py', 'encoder.py', 'Home.py', 'perform_fun.py', 'test.sqlite3']
+includes = []
+excludes = ['tkinter','sys','PIL','cv2','sqlite',]
+packages = ['do','khh']
 
-# GUI applications require a different base on Windows (the default is for a
-# console application).
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
-
-setup(  name = "guifoo",
-        version = "0.1",
-        description = "My GUI application!",
-        options = {"build_exe": build_exe_options},
-        executables = [Executable("login_text.py", base=base)])
+setup(
+    name = 'myapp',
+    version = '0.1',
+    description = 'A general enhancement utility',
+    author = 'lenin',
+    author_email = 'le...@null.com',
+    options = {'build_exe': {'includes':includes,'excludes':excludes,'packages':packages,'include_files':includefiles}}, 
+    executables = [Executable('login.py.py')]
+)
